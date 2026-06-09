c03650564fa43f0b74fa91a75aa6f14f0800cf89
s-irlui <118243008+s-irlui@users.noreply.github.com>
2026-06-09 13:14:44 +0300

    Implement CashRegister class with methods

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,61 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, discount):
+        if isinstance(discount, int) and discount >= 0 and discount <= 100:
+            self._discount = discount
+        else:
+            print("Not valid discount")
+            self._discount = 0
+
+    def add_item(self, item, price, quantity):
+        transaction_total = price * quantity
+
+        self.total += transaction_total
+
+        for i in range(quantity):
+            self.items.append(item)
+
+        transaction = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+
+        self.previous_transactions.append(transaction)
+
+    def apply_discount(self):
+        if len(self.previous_transactions) == 0:
+            print("There is no discount to apply.")
+            return
+
+        discount_amount = self.total * (self.discount / 100)
+        self.total -= discount_amount
+
+        self.previous_transactions.pop()
+
+    def void_last_transaction(self):
+        if len(self.previous_transactions) == 0:
+            print("There is no transaction to void.")
+            return
+
+        last_transaction = self.previous_transactions.pop()
+
+        item = last_transaction["item"]
+        price = last_transaction["price"]
+        quantity = last_transaction["quantity"]
+
+        self.total -= price * quantity
+
+        for i in range(quantity):
+            if item in self.items:
+                self.items.remove(item)
